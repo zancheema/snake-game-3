080e5f285e89d8edcfdd80674d7b24f7e56c9be2
= <social.zaincheema@gmail.com>
2021-03-14 08:52:40 +0500

    configured bottom nav

--- app/src/main/java/com/example/android/socialnetwork/MainActivity.kt
+++ app/src/main/java/com/example/android/socialnetwork/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.android.socialnetwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /** setup the bottom nav view */
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottomNav)
+            .setupWithNavController(navController)
     }
 }
